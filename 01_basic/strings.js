e5f11cf8ad2cf3e098cc015d72a2176d3b269576
Anuj singh <100374931+AnujSingh001@users.noreply.github.com>
2023-09-29 17:31:38 +0000

    strings

--- 01_basic/strings.js
+++ 01_basic/strings.js
@@ -8,3 +8,15 @@ console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
 const gameName = new String("Snake game")
 
 console.log(gameName.charAt(0))
+console.log(gameName.indexOf(' '))
+
+const String1 = gameName.slice(0,3)
+console.log(String1)
+
+const string2 = "     Anuj budhwar.  "
+console.log(string2.trim())
+
+const url = "https://AnujBudhwar/teji%20budhwar"
+console.log(url.replace("%20","_"))
+
+console.log(url.includes("Anuj"))
