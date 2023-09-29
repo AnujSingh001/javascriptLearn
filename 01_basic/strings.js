529b91dfca861dfcbb13afcfcd29b12eb5fb3046
Anuj singh <100374931+AnujSingh001@users.noreply.github.com>
2023-09-29 11:22:09 +0000

    strings

---
+++ 01_basic/strings.js
@@ -0,0 +1,10 @@
+const name = "Anuj"
+const repoCount = "123456"
+
+// console.log(name + repoCount)
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String("Snake game")
+
+console.log(gameName.charAt(0))
